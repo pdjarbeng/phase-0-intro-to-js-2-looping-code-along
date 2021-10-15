1be68bfa424d8d48a710f8f36dc796f86029e526
priscilla <pdjarbeng@outlook.com>
2021-10-15 01:51:27 -0400

    commit tree d413913f48dd012fb4aa3fa85a5d3201a818ab65

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const namesArray = ["Ada", "Brendan", "Ali"]
+const event = ("birthday")
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+
+function countDown( number ) {
+while ( number > 0 ) {
+    console.log( number );
+    number -= 1;
+    }
+console.log( number );
+}
